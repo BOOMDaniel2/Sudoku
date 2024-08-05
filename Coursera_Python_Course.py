16f91fb8241cb6d697f7d8ccf2f782ee870127ae
daniel_ljb <95828887+daniel-ljb@users.noreply.github.com>
2024-08-05 22:24:05 +0100

    Update Coursera_Python_Course.py

--- Coursera_Python_Course.py
+++ Coursera_Python_Course.py
@@ -1,16 +1,13 @@
-"""BLAHBLAHBLAH"""
 import datetime as dt
 date = dt.date
 datetime = dt.datetime
 
 def days_in_month(year, month):
-    """BLAHBLAHBLAH"""
     if month == 12:
         return 31
     return (date(year, month+1, 1) - date(year, month, 1)).days
 
 def is_valid_date(year, month, day):
-    """BLAHBLAHBLAH"""
     try:
         datetime(year, month, day)
         return True
@@ -18,7 +15,6 @@ def is_valid_date(year, month, day):
         return False
 
 def days_between(y_1, m_1, d_1, y_2, m_2, d_2):
-    """BLAHBLAHBLAH"""
     if not is_valid_date(y_1, m_1, d_1) or not is_valid_date(y_2, m_2, d_2):
         return 0
     
@@ -29,7 +25,6 @@ def days_between(y_1, m_1, d_1, y_2, m_2, d_2):
         return days
 
 def age_in_days(year, month, day):
-    """BLAHBLAHBLAH"""
     if not is_valid_date(year, month, day):
         return 0
     
